b98809500a3c84954e80b5c79ab4d5eae24b3360
GiovanniSim <giovanni5im2007@gmail.com>
2025-02-23 19:15:59 +0700

    Login and Log Out

---
+++ email.js
@@ -0,0 +1,34 @@
+const email = document.querySelector('#email');
+let emailValue = '';
+
+const cekUser= localStorage.getItem('user');
+
+if (cekUser != null) {
+    document.querySelector('#register').remove();
+}
+
+email.addEventListener('keyup', function(e) {
+    emailValue = e.target.value;
+    console.log('email anda :', emailValue)
+});
+
+const registerForm = document.querySelector('#register');
+
+registerForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    if (emailValue.length == 0) {
+        alert("email tidak dapat dideteksi");
+    } else {
+        alert("xie xie peng you!");
+        localStorage.setItem ('user', emailValue);
+        document.querySelector('#register').remove();
+    }
+});
+
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('click', function (e) {
+    localStorage.removeItem('user');
+    location.reload();
+});
